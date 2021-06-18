0e1b62f707c44fd45c76971dc5d4b8a6fc722926
wansyer <qhusyairi@live.com>
2021-06-18 11:56:23 +0800

    Update smartbin.ino

--- smartbin.ino
+++ smartbin.ino
@@ -1,27 +1,15 @@
-/*
-  Ping))) Sensor
-  This sketch reads a PING))) ultrasonic rangefinder and returns the distance
-  to the closest object in range. To do this, it sends a pulse to the sensor to
-  initiate a reading, then listens for a pulse to return. The length of the
-  returning pulse is proportional to the distance of the object from the sensor.
-  The circuit:
-	- +V connection of the PING))) attached to +5V
-	- GND connection of the PING))) attached to ground
-	- SIG connection of the PING))) attached to digital pin 7
-  created 3 Nov 2008
-  by David A. Mellis
-  modified 30 Aug 2011
-  by Tom Igoe
-  This example code is in the public domain.
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Ping
-*/
+#include <Servo.h>
 
-// this constant won't change. It's the pin number of the sensor's output:
-const int pingPin = 7;
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+const int pingPin = 7;  // wansyer wuz here
+int pos = 0;    // variable to store the servo position
 
 void setup() {
   // initialize serial communication:
   Serial.begin(9600);
+  myservo.attach(9);
 }
 
 void loop() {
