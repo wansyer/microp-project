5d8c320f291f93b1576e65a041c017c1a5f9daa8
wansyer <qhusyairi@live.com>
2021-06-20 13:09:47 +0800

    Update smartbin.ino
    Added Ultrasonic Distance Sensor code sets.
    
    The output is in centimeter. Range 3<x<1 and x<1 could be utilized in if-else cases.

--- smartbin.ino
+++ smartbin.ino
@@ -1,4 +1,5 @@
 const int fsrAnalogPin = A0;    // pin for sensor
+const int pingPin = 7;
 const int ledGreen = 13;       // pin for Green LED
 const int ledRed = 12;       // pin for Red LED
 const int transistor = 2;
@@ -14,9 +15,26 @@ void setup() {
 }
 
 void loop() {
+  // establish variables for duration of the ping, and the distance result
+  // in inches and centimeters:
+  long duration, inches, cm;
   int fsrReading = analogRead(fsrAnalogPin);
   Serial.print("Analog reading = ");
   Serial.println(fsrReading);
+
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
+  cm = microsecondsToCentimeters(duration);
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
 
   // if the distance is less than 3 meter, Green LED is ON
   if (fsrReading < threshold) {
@@ -34,4 +52,11 @@ void loop() {
   // print the analog value:
   Serial.println(analogValue);
   delay(100);        // delay in between reads for stability
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the object we
+  // take half of the distance travelled.
+  return microseconds / 29 / 2;
 }
