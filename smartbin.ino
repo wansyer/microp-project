988b945b44be43a06e1247b04e73499739966d25
wansyer <qhusyairi@live.com>
2021-06-27 12:32:21 +0800

    Update smartbin.ino

--- smartbin.ino
+++ smartbin.ino
@@ -1,9 +1,9 @@
 #include <Servo.h>
 
 Servo myservo;
-int pos=0;
-
-Servo servo_9;
+// Define the servo pin:
+#define servoPin 9
+int angle = 0;
 const int fsrAnalogPin = 0;
 int fsrReading;
 const int pingPin = 6;
@@ -22,7 +22,7 @@ void setup()
 pinMode(ledGreen, OUTPUT);
 pinMode(ledRed, OUTPUT);
 pinMode(ledYellow, OUTPUT);
-servo_9.attach(9); 
+myservo.attach(servoPin);
 pinMode(fsrAnalogPin, INPUT);
 Serial.begin(9600);
 }
@@ -31,8 +31,6 @@ void loop()
 {
 long duration, cm;
 
-  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
   pinMode(pingPin, OUTPUT);
   digitalWrite(pingPin, LOW);
   delayMicroseconds(2);
@@ -40,13 +38,10 @@ long duration, cm;
   delayMicroseconds(5);
   digitalWrite(pingPin, LOW);
 
-  // The same pin is used to read the signal from the PING))): a HIGH
-  // pulse whose duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
   pinMode(pingPin, INPUT);
   duration = pulseIn(pingPin, HIGH);
 
-  // convert the time into a distance
+
   cm = microsecondsToCentimeters(duration);
 
   Serial.print(duration);
@@ -56,33 +51,33 @@ long duration, cm;
   Serial.println();
 
   delay(1000);
-              
 fsrReading = analogRead(fsrAnalogPin); 
 Serial.print("Analog reading = ");
 Serial.println(fsrReading);
   
 if (100 < cm && cm < 300){
+    myservo.write(0);
     fsrReading = analogRead(fsrAnalogPin);
     fsrVoltage = map(fsrReading, 0, 1023, 0, 5000);
 		if (fsrVoltage == 0){
         Serial.println("No pressure");  
         }
-else {       
-    fsrResistance = 5000 - fsrVoltage;     // fsrVoltage is in millivolts so 5V = 5000mV
-    fsrResistance *= 10000;                // 10K resistor
-    fsrResistance /= fsrVoltage;
-    fsrConductance = 1000000;           // we measure in micromhos so 
-    fsrConductance /= fsrResistance;
-    Serial.print("Conductance in microMhos: ");
-    Serial.println(fsrConductance);
-    if (fsrConductance <= 1000)
-	{
-    fsrForce = fsrConductance / 80;
-    Serial.print("Force in Newtons: ");
-    Serial.println(fsrForce);}
-}
+        else {       
+        fsrResistance = 5000 - fsrVoltage;     // fsrVoltage is in millivolts so 5V = 5000mV
+        fsrResistance *= 10000;                // 10K resistor
+        fsrResistance /= fsrVoltage;
+        fsrConductance = 1000000;           // we measure in micromhos so 
+        fsrConductance /= fsrResistance;
+        Serial.print("Conductance in microMhos: ");
+        Serial.println(fsrConductance);
+        if (fsrConductance <= 1000)
+        {
+        fsrForce = fsrConductance / 80;
+        Serial.print("Force in Newtons: ");
+        Serial.println(fsrForce);}
+        }
 
-    if (fsrForce < Minimum) 
+        if (fsrForce < Minimum) 
         {
             digitalWrite(ledYellow, LOW);
             digitalWrite(ledRed, LOW);
@@ -107,7 +102,7 @@ else {
             digitalWrite(ledRed, LOW);
         }
 }
-    else if (0 < cm && cm < 100)
+else if (0 < cm && cm < 100)
     {
         if (fsrForce < Maximum) 
         {
@@ -115,18 +110,10 @@ else {
             digitalWrite(ledRed, LOW);
             digitalWrite(ledGreen, LOW);
             
-            for (pos = 0; pos <= 90; pos += 1) 
-            {
-                myservo.write(pos);
-            delay(15);
-            }
+            myservo.write(90);
             delay(5000);
-            for (pos = 90; pos >= 0; pos -= 1) 
-            {
-                myservo.write(pos);
-            delay(30);
-            }
-        delay(7000);
+            myservo.write(0);
+            delay(1000);
         }
         else
         {
