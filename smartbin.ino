d9e63ef0dff0f7caa98a84a43d8af87a3f3221cc
wansyer <qhusyairi@live.com>
2021-06-27 11:53:20 +0800

    Revision 2
    Know of Not Working
    - servo

--- smartbin.ino
+++ smartbin.ino
@@ -1,88 +1,155 @@
 #include <Servo.h>
 
+Servo myservo;
+int pos=0;
+
 Servo servo_9;
-const int fsrAnalogPin = A0;    // pin for sensor
-int fsrReading = 0;
-const int pingPin = 7; //declare pin 7 for Ultrasonic Distance Sensor
-int ledGreen = 12;       // pin for Green LED
-int ledRed = 13;       // pin for Red LED
-int ledYellow = 11;   // pin for Yellow LED
-int threshold = 300;   // an arbitrary threshold level that's in the range of the analog input
+const int fsrAnalogPin = 0;
+int fsrReading;
+const int pingPin = 6;
+int ledGreen = 12;
+int ledYellow = 11;
+int ledRed = 13;
+const int Minimum = 4.905;
+const int Maximum = 9.81;
+int fsrVoltage; 
+unsigned long fsrResistance;
+unsigned long fsrConductance;
+long fsrForce; 
 
 void setup() 
 {
-  // initialize the LED pin as an output:
-  pinMode(ledGreen, OUTPUT);
-  pinMode(ledRed, OUTPUT);
-  pinMode(ledYellow, OUTPUT);
-  servo_9.attach(9); //set the servo pin 9 as an output pin
-  // initialize the force sensor as an input;
-  pinMode(fsrAnalogPin, INPUT);
-  // initialize serial communications:
-  Serial.begin(9600);
+pinMode(ledGreen, OUTPUT);
+pinMode(ledRed, OUTPUT);
+pinMode(ledYellow, OUTPUT);
+servo_9.attach(9); 
+pinMode(fsrAnalogPin, INPUT);
+Serial.begin(9600);
 }
 
 void loop() 
 {
-  // establish variables for duration of the ping, and the distance result
-  // in inches and centimeters:
-  long duration, inches, cm;
-  int fsrReading = analogRead(fsrAnalogPin); //read value from AnalogPin to fsrReading
-  Serial.print("Analog reading = ");
-  Serial.println(fsrReading);
-  
-  //The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
-  //Give a shot LOW pulse beforehand to ensure a celan HIGH pulse:
+long duration, cm;
+
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
   pinMode(pingPin, OUTPUT);
   digitalWrite(pingPin, LOW);
   delayMicroseconds(2);
   digitalWrite(pingPin, HIGH);
   delayMicroseconds(5);
   digitalWrite(pingPin, LOW);
-  
-  //The same pin is used to read the signal from the PING))): a HIGH pulse
-//whose duration is the time (in microseconds) from the sending of the ping
-//to the reception of its echo off of an object.
+
+  // The same pin is used to read the signal from the PING))): a HIGH
+  // pulse whose duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
   pinMode(pingPin, INPUT);
   duration = pulseIn(pingPin, HIGH);
-  
-  //convert the time into a distance
+
+  // convert the time into a distance
   cm = microsecondsToCentimeters(duration);
+
+  Serial.print(duration);
+  Serial.print(" ms ");
   Serial.print(cm);
   Serial.print("cm");
   Serial.println();
 
+  delay(1000);
+              
+fsrReading = analogRead(fsrAnalogPin); 
+Serial.print("Analog reading = ");
+Serial.println(fsrReading);
   
-  if (cm<2500)
-  {
-  // if the distance is less than 3 meter, Green LED is ON
-  if (fsrReading < threshold) 
-  {
-    digitalWrite(ledGreen, HIGH);
-    // insert another set of instructions below
-    
-
-  }
-  else 
-  {
-    digitalWrite (transistor, HIGH);
-    delay(500);
-    digitalWrite (transistor, LOW);
-    delay(500);
-  }
-  }
-  else if
-  {
-  
-  }
-  // print the analog value:
-  Serial.println(analogValue);
-  delay(100);        // delay in between reads for stability
+if (100 < cm && cm < 300){
+    fsrReading = analogRead(fsrAnalogPin);
+    fsrVoltage = map(fsrReading, 0, 1023, 0, 5000);
+		if (fsrVoltage == 0){
+        Serial.println("No pressure");  
+        }
+else {       
+    fsrResistance = 5000 - fsrVoltage;     // fsrVoltage is in millivolts so 5V = 5000mV
+    fsrResistance *= 10000;                // 10K resistor
+    fsrResistance /= fsrVoltage;
+    fsrConductance = 1000000;           // we measure in micromhos so 
+    fsrConductance /= fsrResistance;
+    Serial.print("Conductance in microMhos: ");
+    Serial.println(fsrConductance);
+    if (fsrConductance <= 1000)
+	{
+    fsrForce = fsrConductance / 80;
+    Serial.print("Force in Newtons: ");
+    Serial.println(fsrForce);}
 }
 
-long microsecondsToCentimeters(long microseconds) {
+    if (fsrForce < Minimum) 
+        {
+            digitalWrite(ledYellow, LOW);
+            digitalWrite(ledRed, LOW);
+            digitalWrite(ledGreen, HIGH);
+            delay(3000);
+            digitalWrite(ledGreen, LOW);
+        }
+        else if (Minimum < fsrForce && fsrForce < Maximum) 
+        {
+            digitalWrite(ledGreen, LOW);
+            digitalWrite(ledRed, LOW);
+            digitalWrite(ledYellow, HIGH);
+            delay(3000);
+            digitalWrite(ledYellow, LOW);
+        }
+        else if (fsrForce > Maximum) 
+        {
+            digitalWrite(ledYellow, LOW);
+            digitalWrite(ledGreen, LOW);
+            digitalWrite(ledRed, HIGH);
+            delay(3000);
+            digitalWrite(ledRed, LOW);
+        }
+}
+    else if (0 < cm && cm < 100)
+    {
+        if (fsrForce < Maximum) 
+        {
+            digitalWrite(ledYellow, LOW);
+            digitalWrite(ledRed, LOW);
+            digitalWrite(ledGreen, LOW);
+            
+            for (pos = 0; pos <= 90; pos += 1) 
+            {
+                myservo.write(pos);
+            delay(15);
+            }
+            delay(5000);
+            for (pos = 90; pos >= 0; pos -= 1) 
+            {
+                myservo.write(pos);
+            delay(30);
+            }
+        delay(7000);
+        }
+        else
+        {
+            digitalWrite(ledGreen, LOW);
+            digitalWrite(ledYellow, LOW);
+            digitalWrite(ledRed, HIGH);
+            delay(1000);
+            digitalWrite(ledRed, LOW);
+            digitalWrite(ledRed, HIGH);
+            delay(1000);
+            digitalWrite(ledRed, LOW);
+            myservo.write(0);
+        }
+    }
+    else{
+        myservo.write(0);
+    }
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
   // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // The ping travels out and back, so to find the distance of the object we
-  // take half of the distance travelled.
-  return microseconds / 29 / 2;
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+return microseconds / 29 / 2;
 }
